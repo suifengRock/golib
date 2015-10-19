d6eff2da9a60254dd0fde9a35e694d1b0be97494
suifengLuo <772396555@qq.com>
2015-10-19 16:37:26 +0800

    add the log color

--- example/log.go
+++ example/log.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	log.Info(" I'm info .")
-	log.Trace(" I'm trace .")
+	log.Debug(" I'm trace .")
 	log.Warning(" I'm warning .")
 	log.Fatal(" I'm fatal .")
 }
--- log/log.go
+++ log/log.go
@@ -1,70 +1,140 @@
 package log
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
 )
 
-type Logger struct {
-	*log.Logger
-	debug  bool
-	prefix string
+// terminal color
+var (
+	green   = string([]byte{27, 91, 57, 55, 59, 52, 50, 109})
+	white   = string([]byte{27, 91, 57, 48, 59, 52, 55, 109})
+	yellow  = string([]byte{27, 91, 57, 55, 59, 52, 51, 109})
+	red     = string([]byte{27, 91, 57, 55, 59, 52, 49, 109})
+	blue    = string([]byte{27, 91, 57, 55, 59, 52, 52, 109})
+	magenta = string([]byte{27, 91, 57, 55, 59, 52, 53, 109})
+	cyan    = string([]byte{27, 91, 57, 55, 59, 52, 54, 109})
+	reset   = string([]byte{27, 91, 48, 109})
+)
+
+// log level
+const (
+	NOTSET   = 0
+	DEBUG    = 10
+	INFO     = 20
+	WARNING  = 30
+	ERROR    = 40
+	CRITICAL = 50
+)
+
+func getLevelStrAndColor(level uint32) (string, string) {
+	switch level {
+	case DEBUG:
+		return "[Debug]", magenta
+	case INFO:
+		return "[INFO]", green
+	case WARNING:
+		return "[Warning]", yellow
+	case ERROR:
+		return "[Error]", red
+	case CRITICAL:
+		return "[Critical]", cyan
+	default:
+		return "", ""
+	}
 }
 
-func New(out io.Writer, prefix string, debug bool, flag int) *Logger {
-	l := &Logger{debug: debug, prefix: prefix}
-	l.Logger = log.New(out, "", flag)
+type Logger struct {
+	*log.Logger
+	enableColor bool
+	level       uint32
+	calldepth   int
+}
+
+func New(out io.Writer, prefix string, color bool, level uint32, flag int) *Logger {
+	l := &Logger{enableColor: color, level: level}
+	l.calldepth = 4
+	l.Logger = log.New(out, prefix, flag)
 	return l
 }
 
+func (l *Logger) SetCalldepth(calldepth int) {
+	l.calldepth = calldepth
+}
+
+func (l *Logger) SetLevel(level uint32) {
+	l.level = level
+}
+
+func (l *Logger) SetEnableColor(enableColor bool) {
+	l.enableColor = enableColor
+}
+
+func (l *Logger) content(level uint32, v string, args ...interface{}) {
+	levelStr, levelColor := getLevelStrAndColor(level)
+	var content string
+	if l.enableColor {
+		content = fmt.Sprintf("%s%s%s\t%s", levelColor, levelStr, reset, fmt.Sprintf(v, args...))
+	} else {
+		content = fmt.Sprintf("%s\t%s", levelStr, fmt.Sprintf(v, args...))
+
+	}
+	l.print(content)
+}
 func (l *Logger) print(v string) {
-	if !l.debug {
+	l.Output(l.calldepth, v)
+}
+
+func (l *Logger) Debug(v string, args ...interface{}) {
+	if l.level > DEBUG {
 		return
 	}
-	l.Output(3, v)
+	l.content(DEBUG, v, args...)
 }
 
-func (l *Logger) Trace(v string) {
-
-	str := "[Trace]    " + l.prefix + v
-	l.print(str)
+func (l *Logger) Info(v string, args ...interface{}) {
+	if l.level > INFO {
+		return
+	}
+	l.content(INFO, v, args...)
 }
 
-func (l *Logger) Info(v string) {
-
-	str := "[Info]     " + l.prefix + v
-	l.print(str)
+func (l *Logger) Warning(v string, args ...interface{}) {
+	if l.level > WARNING {
+		return
+	}
+	l.content(WARNING, v, args...)
 }
 
-func (l *Logger) Warning(v string) {
-	str := "[Warning] " + l.prefix + v
-	l.print(str)
+func (l *Logger) Fatal(v string, args ...interface{}) {
+	if l.level > CRITICAL {
+		return
+	}
+	l.content(CRITICAL, v, args...)
+	panic("log fatal")
 }
 
-func (l *Logger) Fatal(v string) {
-	str := "[Fatal]   " + l.prefix + v
-	l.print(str)
+var logger = New(os.Stderr, "[App] ", true, DEBUG, log.Lshortfile|log.LstdFlags)
+
+func init() {
+	logger.SetCalldepth(5)
 }
 
-var logger = New(os.Stderr, "", true, log.Lshortfile|log.LstdFlags)
+func Debug(v string, args ...interface{}) {
 
-func Trace(v string) {
-	str := "[Trace]    " + logger.prefix + v
-	logger.print(str)
+	logger.Debug(v, args...)
 }
 
-func Info(v string) {
-	str := "[Info]     " + logger.prefix + v
-	logger.print(str)
+func Info(v string, args ...interface{}) {
+	logger.Info(v, args...)
 }
 
-func Warning(v string) {
-	str := "[Warning] " + logger.prefix + v
-	logger.print(str)
+func Warning(v string, args ...interface{}) {
+	logger.Warning(v, args...)
 }
 
-func Fatal(v string) {
-	str := "[Fatal]   " + logger.prefix + v
-	logger.print(str)
+func Fatal(v string, args ...interface{}) {
+	logger.Fatal(v, args...)
 }
